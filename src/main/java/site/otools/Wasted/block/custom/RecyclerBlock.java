9eba2a886dbc654efbe77c37d3e32cf92dbe1527
Vadim Latisev <Vadimhd39@gmail.com>
2026-03-24 19:01:53 +0200

    added plastic recycler textures and additional stuff
    Block faces player when placed. Rotated it in players hand so it looks less awkward.
    Didn't get it to load all side textures from a single file

--- src/main/java/site/otools/Wasted/block/custom/RecyclerBlock.java
+++ src/main/java/site/otools/Wasted/block/custom/RecyclerBlock.java
@@ -3,6 +3,7 @@ package site.otools.Wasted.block.custom;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -11,13 +12,18 @@ import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.Interaction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import site.otools.Wasted.block.entity.ModBlockEntities;
@@ -25,14 +31,27 @@ import site.otools.Wasted.block.entity.RecyclerBlockEntity;
 
 public class RecyclerBlock extends BaseEntityBlock {
     public static final MapCodec<RecyclerBlock> CODEC = simpleCodec(RecyclerBlock::new);
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public RecyclerBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
     protected MapCodec<? extends BaseEntityBlock> codec() {
         return null;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
@@ -81,6 +100,4 @@ public class RecyclerBlock extends BaseEntityBlock {
         return createTickerHelper(blockEntityType, ModBlockEntities.RECYCLER_BE.get(),
                 (level1, blockPos, blockState, blockEntity) ->blockEntity.tick(level1, blockPos, blockState));
     }
-
-
 }
